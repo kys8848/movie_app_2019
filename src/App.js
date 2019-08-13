5637f060dcab3dac445bf26f38e40936f3a946aa
unknown <YS>
2019-08-14 00:40:07 +0900

    4.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 
 class App extends React.Component {
@@ -7,10 +8,14 @@ class App extends React.Component {
     movies: []
   };
 
-  componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading:false});
-    }, 6000);
+  getMovies = async () =>{
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  // getMovie fucnsiton에게 시간이 조금 필요하고 그걸 기다려 달라고 말해야함(원전히 불러드리기 까지)
+  }
+
+
+componentDidMount(){ //async를 붙이는 이유 : componentDidMount가 완전히 동작히 끝날때 까지 약간 시간이 거릴 수 있다고 말해야함 그걸해줌
+    this.getMovies(); // 이런식으로 하면 컴포넌트가 마운트 되면 겟 무비를 호출함
   }
 
   render(){
