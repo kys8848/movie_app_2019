f525e8b7ff58e6e0ab7a8cd7bc3923ba2ec7eb6d
unknown <YS>
2019-08-14 00:25:16 +0900

    3.1~3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
 
+class App extends React.Component {
+  state = {
+    isLoading:true,
+    movies: []
+  };
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ isLoading:false});
+    }, 6000);
+  }
+
+  render(){
+    const { isLoading } = this.state;
+    return <div>{isLoading ? ":Loading..." : "we are ready"}</div>
+  }
+}
+
+
+/*
 function App() {
   return <div>안녕 오소고날!</div>;
     //<div className="App" />;
     
   
-}
+}*/
 
 export default App;
